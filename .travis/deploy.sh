156bf5511e7d72b7004df852d0350644c6a91a3c
khs1994 <khs1994@khs1994.com>
2017-11-19 00:36:16 +0800

    Update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
-git clone -b gitbook $REPO repo
-cd repo
+
 git ls-files | while read file; do touch -d $(git log -1 --format="@%ct" "$file") "$file"; done
+
 docker run -it --rm \
   -v $PWD:/srv/gitbook-src \
   khs1994/gitbook
 
-# Clone the repository
-git clone -b "$DEPLOY_BRANCH" "$REPO" .deploy_git
-if [ ! $? = 0 ];then
-  #不存在
-  echo -e "\033[32mINFO\033[0m  BRANCH NOT exist"
-  mkdir .deploy_git
-  cd .deploy_git
-  git init
-  git remote add origin $REPO
-  cd ..
-else
-  #存在
-  echo -e "\033[32mINFO\033[0m  BRANCH exist"
-  rm -rf .deploy_git/*
-fi
-# Deploy to GitHub and aliyun
-sudo cp -a _book/* .deploy_git/
-cd .deploy_git
+sudo chmod -R 777 _book
+cd _book
+git init
+git remote add origin "$REPO"
 git remote add aliyun "$REPO_ALIYUN"
-git checkout -b "$DEPLOY_BRANCH"
 git add .
 COMMIT=`date "+%F %T"`
 git commit -m "Travis CI Site updated: $COMMIT"
-git push -f aliyun "$DEPLOY_BRANCH"
-git push -f origin "$DEPLOY_BRANCH"
-cd ~
-sudo rm -rf repo
+git push -f aliyun master:"$DEPLOY_BRANCH"
+git push -f origin master:"$DEPLOY_BRANCH"
